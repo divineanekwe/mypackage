16290f067cbe84bbdd26739152738363b5de1d57
Divine Anekwe <divine.anekwe.chukwubuikem@gmail.com>
2025-10-19 18:51:58 +0100

    fix (metadata): Update version to 0.3 and correct Home-page URL

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'mypackage',
-    version = '0.2',
+    version = '0.3',
     packages = find_packages(exclude = ['tests*']),
     lisence = 'MIT',
     description = 'EDSA example python package',
     long_description = open('README.md').read(),
     install_requires = ['numpy'],
-    url = 'https://github.com/<divineanekwe>/mypackage>',
+    url = 'https://github.com/divineanekwe/mypackage',
     author = 'Divine Anekwe',
     author_email = 'divine.anekwe.chukwubuikem@gmail.com'
 )
